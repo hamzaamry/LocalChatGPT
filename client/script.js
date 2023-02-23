546443421354e0b08ae1380953824e135d4d62f2
hamzaamry <hamzaamry009@gmail.com>
2023-02-23 23:48:40 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://localchatgpt.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
